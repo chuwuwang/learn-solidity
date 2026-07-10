a8957926b9ef3de3760fe3588dd6631d5c32ced5
chuwuwang <chuwuwang@126.com>
2026-07-10 14:41:30 +0800

    Update FoodInfoItem.sol

--- grammar/FoodInfoItem.sol
+++ grammar/FoodInfoItem.sol
@@ -2,16 +2,28 @@
 pragma solidity ^0.8.30;
 
 contract FoodInfoItem {
+
     struct Food {
         string name;
         string currentTraceName;
         uint8 quality;
         uint8 status;
         address producer;
+        
         bool exists;
+
+        TraceInfo[] traceInfos;
+        mapping(address => bool) purchasedUsers;
     }
 
-    address private _owner;
+    struct TraceInfo {
+        uint timestamp;
+        string traceName;
+        address traceAddress;
+        uint8 traceQuality;
+    }
+
+    address public _owner;
 
     mapping(uint => Food) public _foods;
 
@@ -24,12 +36,7 @@ contract FoodInfoItem {
         _owner = msg.sender;
     }
 
-    function addNewFood(
-        uint foodId,
-        string memory name,
-        string memory currentTraceName,
-        uint8 quality
-    ) public onlyOwner {
+    function addNewFood(uint foodId, string memory name, string memory currentTraceName, uint8 quality) public onlyOwner {
         require(_foods[foodId].exists == false, "Food already exists");
         Food storage newFood = _foods[foodId];
         newFood.name = name;
@@ -40,27 +47,27 @@ contract FoodInfoItem {
         newFood.currentTraceName = currentTraceName;
     }
 
-    function getFood(
-        uint foodId
-    )
-        public
-        view
-        returns (
-            string memory name,
-            string memory currentTraceName,
-            uint8 quality,
-            uint8 status,
-            address producer
-        )
-    {
+    function getFood(uint foodId) public view returns (string memory name, string memory currentTraceName, uint8 quality, uint8 status, address producer) {
         require(_foods[foodId].exists, "Food does not exists");
-        Food memory newFood = _foods[foodId];
-        return (
-            newFood.name,
-            newFood.currentTraceName,
-            newFood.quality,
-            newFood.status,
-            newFood.producer
-        );
+
+        Food storage newFood = _foods[foodId];
+        
+        return (newFood.name, newFood.currentTraceName, newFood.quality, newFood.status, newFood.producer);
     }
+
+    function addTraceInfoByDistributor(uint foodId, string memory traceName, address distributor, uint8 quality) public onlyOwner returns (bool) {
+        require(_foods[foodId].exists, "Food does not exist");
+        require(quality <= 10, "Quality exceeds the limit of quality");
+
+        Food storage food = _foods[foodId];
+
+        require(food.status == 0, "The product is already produced");
+
+        TraceInfo memory traceInfo = TraceInfo(block.timestamp, traceName, distributor, quality);
+        food.traceInfos.push(traceInfo);
+        food.currentTraceName = traceName;
+        food.status = 1;
+        return true;
+    }
+    
 }
